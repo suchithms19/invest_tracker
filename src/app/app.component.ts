7bfd073a8ae105377d9925335fdd76600e5c9b76
suchithms19 <mssuchith2@gmail.com>
2025-09-17 11:51:53 +0530

    feat: add user input components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,UserComponent],
 })
 export class AppComponent {}
