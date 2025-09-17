1d7a7946001e0229929d4aa1136f164b3af78e8e
suchithms19 <mssuchith2@gmail.com>
2025-09-17 12:14:23 +0530

    feat: add investment results calculation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,42 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
 
+interface AnnualDataSchema {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [HeaderComponent,UserComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  
+  calculateInvestmentResults(data: AnnualDataSchema) {
+  const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+  const annualData = [];
+  let investmentValue = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    const totalInterest =
+      investmentValue - annualInvestment * year - initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
+}
